f638cdaadfb57ebd683bd35e8932f9fab463be7a
Alejandra Gómez <alejita.9813.dg@gmail.com>
2019-03-27 12:41:24 -0500

    commit inicial estructura carpetas

---
+++ src/main/java/edu/eci/arsw/randomSpeakers/randomSpeakers/RandomSpeakersApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.randomSpeakers.randomSpeakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomSpeakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomSpeakersApplication.class, args);
+	}
+
+}
