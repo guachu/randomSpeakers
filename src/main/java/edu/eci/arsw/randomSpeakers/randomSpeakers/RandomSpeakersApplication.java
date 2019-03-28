7be53232546aa5f1e5c79fc5ad383f6e1af64cf9
Alejandra Gómez <alejita.9813.dg@gmail.com>
2019-03-27 22:47:12 -0500

    chat lógica

--- src/main/java/edu/eci/arsw/randomSpeakers/randomSpeakers/RandomSpeakersApplication.java
+++ src/main/java/edu/eci/arsw/randomSpeakers/randomSpeakers/RandomSpeakersApplication.java
@@ -9,5 +9,4 @@ public class RandomSpeakersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RandomSpeakersApplication.class, args);
 	}
-
 }
---
+++ src/main/java/edu/eci/arsw/randomSpeakers/randomSpeakers/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.randomSpeakers.randomSpeakers.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+/**
+ *
+ * @author aleja
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
+}
